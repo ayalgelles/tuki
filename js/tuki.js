d7a42f9fe13e55bab3369bebae62d893b2368d59
Eyal Yaniv <eyal@simplified-networks.com>
2012-12-30 17:47:02 +0200

    Added embed youtube form rid param
    Signed-off-by: Eyal Yaniv <eyal@simplified-networks.com>

--- js/tuki.js
+++ js/tuki.js
@@ -25,5 +25,26 @@ function chatController($scope, $defer) {
     $defer(function(){$scope.messages.push({name:message.name, text:message.text});},0);
   });
 }
+var param = function(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var putvideo = function(id){
+    $('<iframe width="560" height="315" src="http://www.youtube.com/embed/"' + id + '" frameborder="0" allowfullscreen></iframe>').appendTo('#video');
+}
+
+
 chatController.$inject = ['$scope','$defer'];
-​
+
+$(document).ready(function() {  
+    var rid = param("rid");
+    putvideo(rid);
+});
