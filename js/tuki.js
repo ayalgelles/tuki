e74dbf8b277e34b251dcbb78b720dbaf7d39de26
ayalgelles <ayal.gelles@simplified-networks.com>
2012-12-30 21:48:31 +0200

    db structure, force connect, create room per invited fbids

--- js/tuki.js
+++ js/tuki.js
@@ -1,47 +1,63 @@
-function param(name)
-{
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regexS = "[\\?&]" + name + "=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.search);
-  if(results === null)
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
+RoomReady = $.Deferred();
+
+window.room = param('rid');
+if (window.room) {
+    RoomReady.resolve();
 }
 
+FBStatus.done(function(stat) {
+		  if (stat !== 'connected') {
+		      $('.fbmodal').modal({backdrop: true}).show();
+		      $('.fbcnt').click(function(){
+					    fblogin();
+					});
+		  }
+		  FBLoggedIn.done(function(resp){
+				      FB.ui({method: 'apprequests',
+					     message: 'wants to watch a video with you on Tuki'
+					    }, function(res){
+						window.room = $.merge([FB.getUserID()], [res.to])
+						    .sort(function(a,b){
+							      return parseInt(a) - parseInt(b);
+							  }).join('-');
+						console.log('room is', window.room);
+						RoomReady.resolve();
+						$('.fbmodal').modal('hide');								  
+					    });
+				      
+				  });
+
+	      });
+
 function chatController($scope, $defer) {
-   
-  $scope.firebaseRef = new Firebase('http://ayal.firebaseio.com/' + param('rid'));
-  $scope.name = '';
-  $scope.text = '';
-  $scope.messages = [];
-  $scope.addMessage = function() {
-    $scope.firebaseRef.push({name:$scope.name, text:$scope.text});
-    $scope.text = '';
-  };
-  $scope.firebaseRef.on('child_added', function(snapshot){
-    var message = snapshot.val();
-    $defer(function(){$scope.messages.push({name:message.name, text:message.text});},0);
-  });
+    $.when(FBLoggedIn, RoomReady).done(function(){
+	var x = new Firebase('http://ayal.firebaseio.com/' + FB.getUserID());
+	x.child(window.room).set({date: (new Date()).getTime()});
+
+	$scope.firebaseRef = new Firebase('http://ayal.firebaseio.com/' + window.room);
+	$scope.name = '';
+	$scope.text = '';
+	$scope.messages = [];
+
+	$scope.addMessage = function() {
+	    $scope.firebaseRef.push({name:$scope.name, text:$scope.text});
+	    $scope.text = '';
+	};
+
+	$scope.firebaseRef.on('child_added', function(snapshot){
+				  var message = snapshot.val();
+				  $defer(function(){
+					     $scope.messages.push({name:message.name, text:message.text});
+					 }, 0);
+			      });
+
+    });
 }
 chatController.$inject = ['$scope','$defer'];
 
-var param = function(name)
-{
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regexS = "[\\?&]" + name + "=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.search);
-  if(results == null)
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
 var putvideo = function(id){
     $('<iframe width="560" height="315" src="http://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>').appendTo('#video');
-}
+};
 
 $(document).ready(function() {  
     var rid = param("rid");
